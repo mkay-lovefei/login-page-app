ac08e1550cfaa2249c3818a2ae1043b3f49cd051
adwoa <mkay.lovefei@gmail.com>
2024-06-20 15:57:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-//import logo from './logo.svg';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import Header from './Header';
-import Footer from "./Footer";
+import Footer from './Footer';
+import './App.css'
 
 function App() {
     return (
         <div className={"App"}>
             <Header />
+            <section className={"content-section"}>
+                <h1>Please enter your User ID</h1>
+            </section>
             <Footer />
         </div>
     );
